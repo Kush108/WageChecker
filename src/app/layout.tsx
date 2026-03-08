095fe85249a0f799aa6c067c23c814428f88bf5b
Kushal Grover <kushal.grover@hstone.com>
2026-03-07 23:44:02 -0700

    Metadata update v2.2

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google"
 import "./globals.css"
 import { Analytics } from "@vercel/analytics/next"
 
+
 const inter = Inter({
   subsets: ["latin"],
   display: "swap"
